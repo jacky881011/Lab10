eaf67f697881700a74a0474e99b36db4db103d99
jacky <jacky.hsu.tw1@gmail.com>
2020-12-11 17:46:19 +0800

    1211

---
+++ app/src/main/java/com/example/lab10/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lab10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private final static int REQUEST_PERMISSION
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
